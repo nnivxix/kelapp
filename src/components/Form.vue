a8df507f281ad5f48db179d3e226fa5031112ff5
nnivxix <adhe.hanasa@gmail.com>
2021-04-03 05:28:32 +0700

    adding style

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,32 +1,45 @@
 <template>
-	<c-flex p="30px">
-		<c-box w="50%" maxH="270px">
-			<c-text fontSize="1.3em">Masukan Nama</c-text>
-			<c-textarea h="40vh" v-model="anggota" placeholder="Masukan nama seluruh siswa" />
-			<c-text>{{jadiArray}}</c-text>
-			<c-text>{{arrayAnggota}}</c-text>
-		</c-box >
-		<c-flex direction="column" alignItems="flex-end" w="50%" >
-			<c-image  :src=img alt="kerjasama"/>
-			<c-stack :spacing="5" p="1em" is-inline align="flex-end" class="control">
-				<c-form-control>
-					<c-form-label for="kelompok">Jumlah Kelompok</c-form-label>
-					<c-input type="Number" v-model="jumlahAnggota"  id="kelompok"  />
-				</c-form-control>
-				<c-button px="3em" variant-color="green" @click="buatKelompok()">Buat Kelompok</c-button>
-			</c-stack>
-			
+	<c-flex direction="column">
+		<c-flex p="30px" :direction="[ 'column', 'row', 'row']">
+			<c-box :w="['100%', '50%']" maxH="270px">
+				<c-text fontSize="1.3em">Masukan Nama</c-text>
+				<c-textarea h="40vh" v-model="anggota" placeholder="Masukan nama seluruh siswa" />
+				<c-text :fontSize="['0.7em', '0.9em']" color="gray.500">Masukan nama kemudian pisahkan dengan koma (,) misal: "Koko, Robert"</c-text>
+			</c-box >
+			<c-flex direction="column" alignItems="flex-end" w="50%" >
+				<c-image :display="['none','block']"  :src=img alt="kerjasama"/>
+				<c-stack :spacing="5" p="1em"  align="flex-end" class="control">
+					<c-form-control :paddingTop="['1em', '0em']">
+						<c-form-label for="kelompok">Jumlah Kelompok</c-form-label>
+						<c-input type="Number" v-model="jumlahAnggota"  id="kelompok" keyup="buatKelompok()" />
+					</c-form-control>
+					<c-button px="3em" variant-color="green" @click="buatKelompok()">Buat Kelompok</c-button>
+				</c-stack>
+			</c-flex>
 		</c-flex>
+
+		<!-- result -->
+		<c-box>
+			<c-text fontSize="1.5em" fontWeight="700" textAlign="center">Kelompok Belajar</c-text>
+			<c-flex flexWrap="wrap" justifyContent="center">
+				<c-box p="3" v-for="(data, index) in arrayAnggota " :key="index">
+					<c-text fontSize="1em" fontWeight="bold">Kelompok {{ index + 1}}</c-text>
+					<c-box v-for="(d, index) in data" :key="index">
+						<c-text>{{d}}</c-text>
+					</c-box>			
+				</c-box>
+			</c-flex>
+		</c-box>
 	</c-flex>
 </template>
 
 
 <script>
-import { CText, CTextarea, CFlex, CBox, CInput, CButton, CStack, CFormControl,  CFormLabel, CImage  } from '@chakra-ui/vue'
+import { CText, CTextarea, CFlex, CBox, CInput, CButton, CStack, CFormControl,  CFormLabel, CImage,   } from '@chakra-ui/vue'
 import img from '../assets/kerjasama.svg'
 export default{
 	name: 'Form',
-	components:{CTextarea, CText, CFlex, CBox, CInput, CButton, CStack, CFormControl,  CFormLabel,  CImage},
+	components:{CTextarea, CText, CFlex, CBox, CInput, CButton, CStack, CFormControl,  CFormLabel,  CImage, },
 	data(){
 		return{
 			anggota:'',

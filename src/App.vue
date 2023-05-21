f51077cb50045eb64e1f5e6b2fd5ef00e022fe2e
nnivxix <adhe.hanasa@gmail.com>
2023-05-21 10:57:26 +0700

    feat: add typescript

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <h1>Hai Vue 3</h1>
+  <p>{{ count }}</p>
+  <button @click="count++">add counter</button>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref<number>(0);
+</script>

868037094faa77b23bdb1cbe794323037e9c3df6
nnivxix <adhe.hanasa@gmail.com>
2023-05-21 10:37:08 +0700

    fix: resolve extention

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from "path";
 export default defineConfig({
   plugins: [vue()],
   resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },

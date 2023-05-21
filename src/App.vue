9be72f058a7551803ac7ec0281e197a613b28143
nnivxix <adhe.hanasa@gmail.com>
2023-05-21 18:40:46 +0700

    feat: footer component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <Navbar></Navbar>
+  <Navbar />
   <div class="flex flex-col md:flex-row mx-6">
     <Form />
-    <Result></Result>
+    <Result />
   </div>
+  <Footer />
 </template>
 
 <script setup lang="ts">
 import Form from "@/components/Form.vue";
 import Result from "./components/Result.vue";
 import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 </script>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,12 @@
 <template>
-	<c-flex :fontSize="['0.8em','1.1em']" marginTop="3em" marginBottom="3em" color="gray.500" justifyContent="center">
-		<c-text>Made with 💚 by <a target="_blank" href="https://twitter.com/nnivxix">Hanasa</a> in 🇮🇩 &#169; 2021</c-text>
-	</c-flex>
+  <footer
+    class="text-xl absolute bottom-0 w-full mt-12 p-20 bg-gray-500 flex justify-center">
+    <p class="text-gray-200">
+      Made with 💚 by
+      <a target="_blank" class="underline" href="https://twitter.com/nnivxix"
+        >Hanasa</a
+      >
+      in 🇮🇩 &#169; 2022 - v.2
+    </p>
+  </footer>
 </template>
-
-
-<script>
-import { CFlex, CText,  } from "@chakra-ui/vue";
-export default{
-	name: 'Footer',
-	components: {CFlex, CText, }
-};
-</script>
-
-
-<style scoped>
-	a{
-		text-decoration: underline;
-	}
-</style>
-		

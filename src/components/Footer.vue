8780b92dbd04b524b98930443bd52387e4120dbd
nnivxix <adhe.hanasa@gmail.com>
2023-05-23 17:22:22 +0700

    feat: link donasi

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,19 @@
 <template>
   <footer
-    class="text-xl relative w-full mt-12 px-3 py-10 md:p-20 bg-gray-500 flex justify-center">
+    class="text-xl relative w-full mt-12 px-3 py-10 md:p-20 bg-gray-500 flex flex-col justify-center">
     <p class="text-gray-200 text-center">
       Made with 💚 by
       <a target="_blank" class="underline" href="https://twitter.com/nnivxix"
         >Hanasa</a
       >
       in 🇮🇩 &#169; 2023 - v.2
+    </p>
+    <p class="text-center text-red-50">
+      Klik link untuk ber-<a
+        class="underline"
+        href="https://www.nihbuatjajan.com/nnivxix"
+        >donasi</a
+      >
     </p>
   </footer>
 </template>

6e159ee07ad260507f566c97d9deaaf03c1dde17
nnivxix <adhe.hanasa@gmail.com>
2023-05-27 11:30:54 +0700

    feat: persistedstate

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+
 import "./style.css";
 import App from "./App.vue";
 
 const pinia = createPinia();
 const app = createApp(App);
 
+pinia.use(piniaPluginPersistedstate);
 app.use(pinia);
 app.mount("#app");
--- src/stores/group.ts
+++ src/stores/group.ts
@@ -1,16 +1,24 @@
 import { ref } from "vue";
 import { defineStore } from "pinia";
 
-const useGroupStore = defineStore("group", () => {
-  const amount = ref("2");
-  const participants = ref("");
-  const groups = ref<any[]>([]);
+const useGroupStore = defineStore(
+  "group",
+  () => {
+    const amount = ref("2");
+    const participants = ref("");
+    const groups = ref<any[]>([]);
 
-  return {
-    amount,
-    participants,
-    groups,
-  };
-});
+    return {
+      amount,
+      participants,
+      groups,
+    };
+  },
+  {
+    persist: {
+      key: "group-store",
+    },
+  }
+);
 
 export default useGroupStore;

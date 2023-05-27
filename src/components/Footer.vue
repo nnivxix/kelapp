528768a6a765713e0fd9ba248d6af2cc78d84a86
nnivxix <adhe.hanasa@gmail.com>
2023-05-27 11:42:45 +0700

    fix: footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,13 +3,20 @@
     class="text-xl relative w-full mt-12 px-3 py-10 md:p-20 bg-gray-500 flex flex-col justify-center">
     <p class="text-gray-200 text-center">
       Made with 💚 by
-      <a target="_blank" class="underline" href="https://twitter.com/nnivxix"
+      <a
+        target="_blank"
+        class="underline"
+        rel="noopener"
+        href="https://twitter.com/nnivxix"
         >Hanasa</a
       >
-      in 🇮🇩 &#169; 2023 - v.2
+      in 🇮🇩 &#169; 2023
     </p>
     <p class="text-center text-red-50">
-      Klik link untuk ber-<a
+      klik di sini untuk ber-<a
+        title="Banyak terima kasih jika Anda berdonasi di sini"
+        rel="noopener"
+        target="_blank"
         class="underline"
         href="https://www.nihbuatjajan.com/nnivxix"
         >donasi</a
